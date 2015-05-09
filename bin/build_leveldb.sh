0dcc15a4f80442ebf5f9fcebcbb2fe64ece548e5
Maxim Dobryakov <maxim.dobryakov@gmail.com>
2015-05-09 15:56:28 +0300

    Fix incorrect path

--- bin/build_leveldb.sh
+++ bin/build_leveldb.sh
@@ -41,7 +41,7 @@ cd $LEVELDB_HOME
 export LIBRARY_PATH=$SNAPPY_HOME/lib
 export C_INCLUDE_PATH=$SNAPPY_HOME/include
 export CPLUS_INCLUDE_PATH=$SNAPPY_HOME/include
-[[ "$OSTYPE" == "msys" ]] && patch -N $LEVELDB_HOME/util/build_detect_platform $ROOT_HOME/patches/leveldb/build_detect_platform.windows.patch
+[[ "$OSTYPE" == "msys" ]] && patch -N $LEVELDB_HOME/build_detect_platform $ROOT_HOME/patches/leveldb/build_detect_platform.windows.patch
 [[ "$OSTYPE" == "msys" ]] && patch -N $LEVELDB_HOME/util/env_posix.cc $ROOT_HOME/patches/leveldb/env_posix.cc.windows.patch
 [[ "$OSTYPE" == "msys" ]] && cp -f $LEVELDB_HOME/patches/leveldb/env_win.cc $ROOT_HOME/util/env_win.cc
 [[ "$OSTYPE" == "msys" ]] && patch -N $LEVELDB_HOME/port/port.h $ROOT_HOME/patches/leveldb/port.h.windows.patch
