11ff0a35ffc0b602e346735222cf149205f538fd
Karasiq <yoba123@yandex.ru>
2020-03-05 12:41:52 +0300

    Remove patches

--- bin/build_leveldb.sh
+++ bin/build_leveldb.sh
@@ -43,11 +43,6 @@ cd $LEVELDB_HOME
 export LIBRARY_PATH=$SNAPPY_HOME/lib
 export C_INCLUDE_PATH=$SNAPPY_HOME/include
 export CPLUS_INCLUDE_PATH=$SNAPPY_HOME/include
-[[ "$OSTYPE" == "msys" ]] && patch -N $LEVELDB_HOME/util/env_posix.cc $ROOT_HOME/patches/leveldb/env_posix.cc.windows.patch
-[[ "$OSTYPE" == "msys" ]] && cp -f $ROOT_HOME/patches/leveldb/env_win.cc $LEVELDB_HOME/util/env_win.cc
-[[ "$OSTYPE" == "msys" ]] && patch -N $LEVELDB_HOME/port/port.h $ROOT_HOME/patches/leveldb/port.h.windows.patch
-[[ "$OSTYPE" == "msys" ]] && cp -f $ROOT_HOME/patches/leveldb/port_win.h $LEVELDB_HOME/port/port_win.h
-[[ "$OSTYPE" == "msys" ]] && cp -f $ROOT_HOME/patches/leveldb/port_win.cc $LEVELDB_HOME/port/port_win.cc
 mkdir -p build && cd build
 
 if [[ "$OSTYPE" == "msys" ]]
